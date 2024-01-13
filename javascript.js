825207a64b2a3f8c09bbb617a8f98e0427df03ed
NaufalArkaan <naufalarkaan25@gmail.com>
2024-01-13 23:54:32 +0800

    first commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// PENGATURAN LOAD
+window.addEventListener('load', function () {
+    document.querySelector('body').classList.add("loaded")
+});
+
+// ALERT PEMBUKAAN
+alert("Welcome To My Website");
